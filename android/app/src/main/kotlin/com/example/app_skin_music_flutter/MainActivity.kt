115551b1c23984a735ff3d3083ea6e95eb945fc0
Ruan <domruan333@gmail.com>
2024-10-08 20:01:20 -0300

    commit inicial

---
+++ android/app/src/main/kotlin/com/example/app_skin_music_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_skin_music_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,221 @@
+// Estrutura básica para um aplicativo de música com Flutter
+
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App de Música',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.redAccent,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _tabs = const [
+    ExplorePage(),
+    LibraryPage(),
+    NowPlayingPage(
+        musicTitle: 'Música Padrão',
+        musicUrl:
+            'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App de Música'),
+      ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.explore),
+            label: 'Explorar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_music),
+            label: 'Biblioteca',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.play_circle_fill),
+            label: 'Tocando Agora',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ExplorePage extends StatelessWidget {
+  const ExplorePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Map<String, String>> musicList = [
+      {
+        'title': 'Música 1 - Artista A',
+        'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3'
+      },
+      {
+        'title': 'Música 2 - Artista B',
+        'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3'
+      },
+      {
+        'title': 'Música 3 - Artista C',
+        'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3'
+      },
+      {
+        'title': 'Música 4 - Artista D',
+        'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3'
+      },
+      {
+        'title': 'Música 5 - Artista E',
+        'url': 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3'
+      },
+    ];
+
+    return ListView.builder(
+      itemCount: musicList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.music_note),
+          title: Text(musicList[index]['title']!),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NowPlayingPage(
+                  musicTitle: musicList[index]['title']!,
+                  musicUrl: musicList[index]['url']!,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class LibraryPage extends StatelessWidget {
+  const LibraryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Sua Biblioteca', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
+
+class NowPlayingPage extends StatefulWidget {
+  final String musicTitle;
+  final String musicUrl;
+
+  const NowPlayingPage(
+      {super.key, required this.musicTitle, required this.musicUrl});
+
+  @override
+  NowPlayingPageState createState() => NowPlayingPageState();
+}
+
+class NowPlayingPageState extends State<NowPlayingPage> {
+  late AudioPlayer _audioPlayer;
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer = AudioPlayer();
+    _audioPlayer.play(widget.musicUrl);
+    isPlaying = true;
+    super.initState();
+    _audioPlayer = AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+
+  void _playPauseMusic() {
+    if (isPlaying) {
+      _audioPlayer.pause();
+    } else {
+      _audioPlayer.play(widget.musicUrl);
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tocando Agora'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              widget.musicTitle,
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.skip_previous),
+                  iconSize: 48,
+                  onPressed: () {},
+                ),
+                IconButton(
+                  icon: Icon(isPlaying ? Icons.pause : Icons.play_arrow),
+                  iconSize: 64,
+                  onPressed: _playPauseMusic,
+                ),
+                IconButton(
+                  icon: const Icon(Icons.skip_next),
+                  iconSize: 48,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
